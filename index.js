b8409d1689bbf218da44a388a2bb3a1d3260b9e3
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-09-26 16:21:07 -0500

    middleware's example.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let exphbs = require('express-handlebars');
 const routes = require('./config/routes');
 let app = express();
 
+function logMiddleware (req,res,next) {
+    console.log(req.url);
+    next();
+};
+
+app.use(logMiddleware);
 app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));

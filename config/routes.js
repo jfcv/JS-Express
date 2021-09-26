1741b8d2d89de4b0eb5808a44d50fcebe68d8fc9
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-09-26 17:00:05 -0500

    'usuario_404.handlebars' added. obtenerUsuario() middleware implemented.

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,8 @@
 let saludador = require('../controllers/saludadorController');
 let userController = require('../controllers/userController');
 
-module.exports = (app) => {
+module.exports = (app, obtenerUsuario) => {
     app.get('/saludo', saludador.saludo);
     app.get('/despedida', saludador.despedida);
-    app.get('/usuario/:id', userController.getUserId);
+    app.get('/usuario/:id', [obtenerUsuario ,userController.getUserId]);
 }
--- index.js
+++ index.js
@@ -8,16 +8,29 @@ function logMiddleware (req,res,next) {
     next();
 };
 
+function obtenerUsuario(req, res, next) {
+    SECONDS = 2;
+    MILS_IN_SECS = 1000;
+    TIME_OUT = SECONDS * MILS_IN_SECS;
+    setTimeout(() => {
+        if(req.params.id === "1") {
+            res.locals.usuario = {nombre: 'Hernan'};
+            next();
+        } else {
+            res.render('usuario_404');
+        }
+    }, TIME_OUT);
+}
+
 app.use(logMiddleware);
 app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+routes(app, obtenerUsuario);
+
 const PORT = 8000;
-
-routes(app);
-
 app.listen(PORT, () => {
     console.log(`Esperando reqs en el puerto ${PORT}`);
 });

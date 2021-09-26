1ad0cf1238cfe2a0ced96a87adec8462f158f4c7
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-09-26 17:31:13 -0500

    using modules in node js through Routers, middlewares, module.param() method.

---
+++ routerExample.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+function obtenerCurso(req, res, next) {
+	setTimeout(function() {
+      res.locals.curso = 'express';
+      next();
+	}, 2000);
+}
+
+var cursos = express.Router();
+
+cursos.param('id', obtenerCurso)
+
+cursos.get('/:id', function(req, res) {
+  res.locals.id = req.params.id;
+  res.render('curso/ver');
+});
+
+cursos.get('/:id/detalles', function(req, res) {
+  res.locals.id = req.params.id;
+  res.render('curso/detalles');
+});
+
+app.use('/cursos', cursos);
+
+app.listen(8000, function () {
+  console.log("Esperando requests en el puerto 8000");
+});
